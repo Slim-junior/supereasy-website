d5ccc0337ea42efddf83b9508a1163ba16ec9b8f
Slim junior <141337467+Slim-junior@users.noreply.github.com>
2024-06-18 13:31:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const textInput = document.getElementById('text');
+const copyButton = document.getElementById('copy');
+
+copyButton.addEventListener('click', ()=> {
+  textInput.select();
+  document.execCommand('copy');
+});
